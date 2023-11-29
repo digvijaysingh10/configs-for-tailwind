3d76017c4d8b3cbc923182d2aec0dbc8a63e213f
DIGVIJAY <70967052+digvijaysingh10@users.noreply.github.com>
2023-11-29 11:22:27 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,77 +1,47 @@
 /** @type {import('tailwindcss').Config} */
-const {nextui} = require("@nextui-org/react");
-
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {
-      borderRadius: {
-        fontFamily: {
-          aesthetic: ["Aesthetic"],
-        },
-        extraLarge: '12rem'
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "2rem", // Default padding for all screen sizes
+        sm: "1rem",      // Padding for small screens
+        md: "2rem",      // Padding for medium screens
+        lg: "3rem",      // Padding for large screens
+        xl: "4rem",      // Padding for extra-large screens
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      screens: {
+        "2xl": "1440px",
+        xl: "1024px",
+        lg: "768px",
+        md: "425px",
+        sm: "375px",
+        xs: "320px",
       },
     },
   },
-  darkMode: "class",
-  plugins: [
-    nextui({
-      layout: {
-        fontFamily: {
-          aesthetic: ["Aesthetic"],
-        }
-      },
-      themes: {
-        light: {
-          colors: {
-            background: "#FFFFFF", // or DEFAULT
-            foreground: "#11181C", // or 50 to 900 DEFAULT
-            primary: {
-              //... 50 to 900
-              foreground: "#FFFFFF",
-              DEFAULT: "#006FEE",
-            },
-            // ... rest of the colors
-          },
-        },
-        dark: {
-          colors: {
-            background: "#000000", // or DEFAULT
-            foreground: "#ECEDEE", // or 50 to 900 DEFAULT
-            primary: {
-              //... 50 to 900
-              foreground: "#FFFFFF",
-              DEFAULT: "#FF6565",
-            },
-            // default:{
-            //   DEFAULT: '#333333'
-            // }
-          },
-          // ... rest of the colors
-        },
-        mytheme: {
-          // custom theme
-          extend: "dark",
-          colors: {
-            primary: {
-              DEFAULT: "#BEF264",
-              foreground: "#000000",
-            },
-            focus: "#BEF264",
-          },
-        },
-      },
-    }),
-  ],
-}
+  plugins: [require("tailwindcss-animate")],
+};
